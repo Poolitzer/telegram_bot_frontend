386a05d28e3be8d616d177123b42edd8c8c98733
Rico <d-Rickyy-b@users.noreply.github.com>
2020-03-25 21:37:34 +0100

    fix: ability to assign psychologist cases as well
    The regex was broken so psychologist cases couldn't be handled at all

--- bot.py
+++ bot.py
@@ -284,7 +284,7 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", deeplink, Filters.regex(r"doctor_\d+")))
-    dispatcher.add_handler(CommandHandler("start", deeplink, Filters.regex(r"^psychologist_\d+$")))
+    dispatcher.add_handler(CommandHandler("start", deeplink, Filters.regex(r"psychologist_\d+$")))
     dispatcher.add_handler(conv_handler)
     dispatcher.add_handler(CallbackQueryHandler(report_handler, pattern=r"^report_\d+$"))
     dispatcher.add_handler(CommandHandler("stop", stop_conversation))
