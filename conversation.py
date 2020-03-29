3d67116159b6cd8594b5746049234cc536d5e8aa
Rico <d-Rickyy-b@users.noreply.github.com>
2020-03-29 20:24:09 +0200

    fix: no need to convert to int anymore

--- conversation.py
+++ conversation.py
@@ -4,10 +4,10 @@
 class Conversation(object):
 
     def __init__(self, worker, user):
-        if int(worker) == int(user):
+        if worker == user:
             raise ValueError("Worker can't be the same as user")
-        self.worker = int(worker)
-        self.user = int(user)
+        self.worker = worker
+        self.user = user
 
     def __repr__(self):
         return "Conversation(worker: {}, user: {})".format(self.worker, self.user)
