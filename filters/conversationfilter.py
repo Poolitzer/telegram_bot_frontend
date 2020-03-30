737dbf7725fb13c82c1561ef4007a30a73f13d7e
Rico <d-Rickyy-b@users.noreply.github.com>
2020-03-30 02:08:55 +0200

    feat: implement conversationfilter
    With this filter, we can now filter for users, which are currently in a conversation of a certain type

---
+++ filters/conversationfilter.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from telegram.ext import BaseFilter
+
+from conversationrequest import ConversationType
+from conversations import Conversations
+
+
+class ConversationFilter(BaseFilter):
+    def filter(self, message):
+        """Returns True if the sender of the message is in a conversation"""
+        conversations = Conversations()
+        return conversations.get_conversation(message.from_user.id) is not None
+
+
+class ConversationTypeFilter(BaseFilter):
+
+    def __init__(self, conversation_type):
+        self.conversation_type = conversation_type
+
+    def filter(self, message):
+        """Returns True if the sender of the message is in a conversation"""
+        conversations = Conversations()
+        conversation = conversations.get_conversation(message.from_user.id)
+        if conversation is None:
+            return False
+
+        return conversation.type == self.conversation_type
+
+
+class MedicalConversationFilter(BaseFilter):
+    def filter(self, message):
+        """Returns True if the sender of the message is in a conversation"""
+        conversations = Conversations()
+        conversation = conversations.get_conversation(message.from_user.id)
+        if conversation is None:
+            return False
+
+        return conversation.type == ConversationType.MEDICAL
+
+
+# Remember to initialize the class.
+conversation_filter = ConversationFilter()
+social_conversation_filter = ConversationTypeFilter(ConversationType.SOCIAL)
+medical_conversation_filter = ConversationTypeFilter(ConversationType.MEDICAL)
